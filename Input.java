6d0dcbb3de52e7cafb0c1055172cac6dc1ff8df6
Dependra Singh <120082076+Dependra027@users.noreply.github.com>
2024-01-03 23:40:54 +0530

    this is how to take input

---
+++ Input.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Input {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        // String input = sc.next();
+        // System.out.println(input);
+
+        // to print full name
+        String name = sc.nextLine();
+        System.out.println(name);
+
+        // to input any integer. Just change the data type to int. i.e int
+        // number=sc.nextInt();
+        // for float, float price=sc.nextFloat();
+    }
+
+}
