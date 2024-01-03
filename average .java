19e6d5be628a0cbfea298a4caebd4fc5dc158f64
Dependra Singh <120082076+Dependra027@users.noreply.github.com>
2024-01-03 23:35:39 +0530

    find average of numbers

---
+++ average .java
@@ -0,0 +1,16 @@
+package
+
+public class average {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.next.Int();
+        int b = sc.next.Int();
+        int c = sc.next.Int();
+
+        int d = (a + b + c) / 3;
+        System.out.println(d);
+
+    }
+
+}
