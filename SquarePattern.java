bce43d5506442daea823d36eeca2eeab01dd3d3b
Dependra Singh <120082076+Dependra027@users.noreply.github.com>
2024-01-10 22:11:22 +0530

    #19 using for loop

---
+++ SquarePattern.java
@@ -0,0 +1,9 @@
+public class SquarePattern {
+    public static void main(String args[]) {
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("* * * *");
+        }
+    }
+
+}
