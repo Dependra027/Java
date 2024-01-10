d191770c0ac33eae71245198bb465504eccf2ba9
Dependra Singh <120082076+Dependra027@users.noreply.github.com>
2024-01-10 21:57:27 +0530

    #14 whileLoop.java

---
+++ whileloop.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class whileloop {
+
+    public static void main(String args[]) {
+
+        int i = 0;
+
+        // syntax of while loop
+        while (i < 100) {
+
+            System.out.println("Hello world");
+            i++;
+        }
+
+    }
+
+}
