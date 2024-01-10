12d918e60eef3367ec60e342137ac9613efe0bbc
Dependra Singh <120082076+Dependra027@users.noreply.github.com>
2024-01-10 22:08:36 +0530

    #18 basic of for loop

---
+++ Forloop.java
@@ -0,0 +1,13 @@
+public class Forloop {
+
+    public static void main(String args[]) {
+
+        // for loop= for(initialisation;condition;updation)
+        // we can use either while or for loop
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello");
+        }
+    }
+
+}
