eacc39c9fb9ca7f5fef2087f6693a523cf270e65
Dependra Singh <120082076+Dependra027@users.noreply.github.com>
2024-01-14 00:24:14 +0530

    #21 what is do while loop

---
+++ doWhileLoop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class doWhileLoop {
+    public static void main(String aargs[]) {
+
+        // in do while , we first do the work and then give the condition
+        int counter = 1;
+        do {
+            System.out.println("Hello world");
+            counter++;
+        } while (counter <= 10);
+    }
+}
