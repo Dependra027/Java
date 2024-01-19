16b6b9fbb2b37f7a3eac0a31e2c83f2ca0870516
Dependra Singh <120082076+Dependra027@users.noreply.github.com>
2024-01-19 10:57:12 +0530

    #34 Call by value1

---
+++ CallByValue.java
@@ -0,0 +1,19 @@
+public class CallByValue {
+
+    public static void main(String args[]) {
+
+        int a = 5;
+        int b = 10;
+
+        // swap
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a= " + a);
+        System.out.println("b= " + b);
+
+    }
+
+}
