0bee1d08abccb5b4d567e74a688ac25a1cc104be
Dependra Singh <120082076+Dependra027@users.noreply.github.com>
2024-02-14 19:43:42 +0530

    #6 Sum of Subarrays

---
+++ 2. Array/SumOfSubarrays.java
@@ -0,0 +1,30 @@
+public class SumOfSubarrays {
+    public static void Sa(int arr[]) {
+        int currSize = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int start = i;
+
+            for (int j = i; j < arr.length; j++) {
+
+                int end = j;
+
+                for (int k = start; k <= end; k++) {
+
+                    currSize += arr[k];
+
+                }
+                System.out.print(currSize + " ");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        Sa(arr);
+    }
+}
